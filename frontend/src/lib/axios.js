e9249efe45f573308cab866813b9a927f497e0c1
Captain AI <47535049+snjugunanjenga@users.noreply.github.com>
2025-09-02 17:58:14 +0000

    Fixing calling issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : import.meta.env.VITE_BACKEND_URL || "https://streamify-video-calls-social-app.onrender.com/api";
+    : import.meta.env.VITE_API_URL || "https://streamify-video-calls-social-app.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
